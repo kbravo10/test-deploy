c3071781c3eac4ebb78685898ac86c500404f065
Kevin Bravo <kbravo1024@gmail.com>
2023-06-20 21:38:06 -0700

    changed Body to SubcriptionCollection

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 //import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
-import Body from "./components/Body";
+import SubcriptionCollection from "./components/SubcriptionCollection";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
+      <SubcriptionCollection />
     </div>
   );
 }
--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -3,12 +3,12 @@ import React from "react";
 function Subscription({ sub }) {
   return (
     <div>
-      <div>
-        <h3>{sub.type}</h3>
-        
-        <span>{sub.price}</span>
-      </div>
-      <img src={sub.logo} />
+      <button>
+        <div>
+          <h3>{sub.type}</h3>
+        </div>
+        <img src={sub.logo} />
+      </button>
     </div>
   );
 }
