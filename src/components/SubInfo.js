0d2ac4292a551510b6c7175f7219b9c84d9906a3
Kevin Bravo <kbravo1024@gmail.com>
2023-06-22 13:41:59 -0700

    add comments to SubInfo

--- src/components/SubInfo.js
+++ src/components/SubInfo.js
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from "react";
 
 function SubInfo({ id }) {
+  //state that holds and sets the data from the fetch request to backend project
   const [subscrption, setSubscription] = useState([]);
 
+  //use fetch method to aquire the data
+  //useEffect to only render once to prevent loop or multiple renders
   useEffect(() => {
     fetch(`http://localhost:3100/subscriptions/${id}`)
       .then((res) => res.json())
       .then((data) => setSubscription((subscrption) => (subscrption = data)));
   }, []);
 
-  console.log(subscrption.logo);
-
+  // return 
   return (
-    <form className="c">
-        <img alt="none" src={subscrption.logo} />
+    <card className="c">
+      <img alt="none" src={subscrption.logo} />
       <h2>{subscrption.type}</h2>
-      
+
       <span className="meta">Price: {subscrption.price}</span>
-    </form>
+    </card>
   );
 }
 
