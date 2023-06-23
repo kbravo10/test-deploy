4407d82e9897f51e7f3165c2629b3fb167f5810b
Kevin Bravo <kbravo1024@gmail.com>
2023-06-22 17:08:51 -0700

    added form and fetch statement

--- src/components/AddSubscription.js
+++ src/components/AddSubscription.js
@@ -1,11 +1,44 @@
+//get the user input for a new subscription and add it to backend of project
+
 import React from "react";
 
-function AddSubscription(){
-    return(
-        <form>
-            <p>add sub</p>
-        </form>
-    )
+function AddSubscription() {
+  //handle the submit button and convert form into an object
+  //insets information into backend db.json project and displays in DOM
+  function handleSubmit(event) {
+    event.preventDefault();
+    const submitForm = Object.fromEntries(new FormData(event.target).entries());
+    fetch(`http://localhost:3100/subscriptions`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(submitForm),
+    }).then((res) => res.json());
+  }
+  return (
+    <form className="add-subscription" onSubmit={handleSubmit}>
+      <div className="type">
+        <label>Name of Subscription: </label>
+        <input type="text" name="type" placeholder="ex: Netflix ..."></input>
+      </div>
+      <div className="price">
+        <label>Price of Subscription</label>
+        <input type="text" name="price" placeholder="ex: 9.99 ..."></input>
+      </div>
+      <div className="logo">
+        <label>Image of logo, if desired: </label>
+        <input
+          type="text"
+          name="logo"
+          placeholder=" insert image adress ..."
+        ></input>
+      </div>
+      <div className="submit">
+        <button type="submit">Submit</button>
+      </div>
+    </form>
+  );
 }
 
 export default AddSubscription;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Header from "./Header";
+
+const styles = {
+  display: "inline-block",
+  width: "150px",
+  padding: "12px",
+  margin: "0 6px 100px",
+  background: "blue",
+  color: "white",
+};
 function NavBar() {
   return (
     <div className="navbar">
-      <NavLink to="/" exact>
+      <NavLink to="/" exact style={styles}>
         Home
       </NavLink>
-      <NavLink to="/subscription">
+      <NavLink to="/subscription" exact style={styles}>
         Subscription
       </NavLink>
-      <NavLink to="/add-sunscription">
+      <NavLink to="/add-sunscription" style={styles}>
         Add Subscription
       </NavLink>
     </div>
